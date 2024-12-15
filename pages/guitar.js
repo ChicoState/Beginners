1d908637c5ef266cec25e04c1f0438781bff9548
sokada <sokada1@csuchico.edu>
2024-12-14 22:26:30 -0800

    delete guiter.js add guitar.js

--- pages/guitar.js
+++ pages/guitar.js
@@ -1,6 +1,7 @@
+'use client';
 import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 import './guitar.css';
-'use client';
+
 neonCursor({
   el: document.getElementById('app'),
   shaderPoints: 16,
